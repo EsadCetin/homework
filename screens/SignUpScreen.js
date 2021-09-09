7b8ab9a8282dbe9f23151e6fa42127fcd597f24e
EsadCetin <mehmetesadcetin@gmail.com>
2021-09-09 12:52:46 +0300

    kahpe cengiz

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -2,28 +2,57 @@ import { View, Text, TouchableOpacity, } from "react-native";
 import React from 'react';
 import styles from './styles.js'
 import { FontAwesome } from '@expo/vector-icons';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Yellowtail_400Regular, Poppins_500Medium, Poppins_400Regular, Moul_400Regular } from '@expo-google-fonts/dev';
 
 function SignUpScreen() {
-    return (
-        <View style={[{ backgroundColor: '#50A162', width: '100%', height: '100%' }]}>
-            <Text style={styles.Text1}>torba</Text>
-            <Text style={styles.Text2}>torbada fırsat var</Text>
-            <View style={[{ marginTop: '15%', marginLeft: '15%', marginRight: '15%', height: '8%', }]}>
-                <TouchableOpacity style={styles.Button}>
-                    <Text style={[{ color: '#fff', fontSize: 18, marginTop: '7%' }]}>Kaydol</Text>
-                </TouchableOpacity>
+    let [fontsLoaded] = useFonts({
+        Yellowtail_400Regular,
+        Poppins_400Regular,
+        Moul_400Regular,
+        Poppins_500Medium
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <View style={[{ backgroundColor: '#50A162', width: '100%', height: '100%' }]}>
+                <Text style={styles.Text1}>torba</Text>
+                <Text style={styles.Text2}>torbada fırsat var</Text>
+                <View style={[{ marginTop: '15%', marginLeft: '15%', marginRight: '15%', height: '8%', }]}>
+                    <TouchableOpacity style={styles.Button}>
+                        <Text style={[{ fontFamily: 'Poppins_400Regular', color: '#fff', fontSize: 18, marginTop: '7%' }]}>Kaydol</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.Text3}>Zaten bir hesabın var mı? Giriş Yap.</Text>
+                <View style={[{ marginTop: '10%', marginLeft: '15%', marginRight: '15%', height: '8%', }]}>
+                    <TouchableOpacity style={styles.Facebook}>
+                        <FontAwesome name="facebook" size={35} color="white"
+                            style={{ marginLeft: '4%' }} />
+                        <Text style={styles.FacebookText}>Facebook ile giriş yap</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.Google}>
+                        <FontAwesome name="google" size={35} color="#EA4335"
+                            style={{ marginLeft: '4%' }} />
+                        <Text style={styles.GoogleText}>Google ile giriş yap</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.Apple}>
+                        <FontAwesome name="apple" size={35} color="white"
+                            style={{ marginLeft: '4%' }} />
+                        <Text style={styles.AppleText}>Apple ile giriş yap</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.Text4}>Sosyal medyada Torba</Text>
+                <View style={[{ marginLeft: '30%', width: '40%', justifyContent: 'center', flexDirection: 'row', }]}>
+                    <FontAwesome name="twitter" size={32} color="#55ACEE"
+                        style={{ marginTop: '4%', }} />
+                    <FontAwesome name="instagram" size={30} color="white"
+                        style={{ marginLeft: '40%', marginTop: '4%', }} />
+                </View>
             </View>
-            <Text style={styles.Text3}>Zaten bir hesabın var mı? Giriş Yap.</Text>
-            <FontAwesome name="facebook" size={35} color="black"
-                style={{ width: '8%', height: '5%', marginLeft: '20%', marginTop: '7%', }} />
-            <FontAwesome name="google" size={35} color="black"
-                style={{ width: '8%', height: '5%', marginLeft: '20%', marginTop: '4%', }} />
-            <FontAwesome name="apple" size={35} color="black"
-                style={{ width: '8%', height: '5%', marginLeft: '20%', marginTop: '4%', }} />
-            <Text style={styles.Text4}>Sosyal medyada Torba</Text>
-            <FontAwesome name="twitter" size={24} color="black"
-                style={{ width: '8%', height: '5%', marginLeft: '30%', marginTop: '4%', }} />
-        </View>
-    );
+
+        );
+    }
 }
 export default SignUpScreen;
--- screens/styles.js
+++ screens/styles.js
@@ -1,19 +1,17 @@
-import { Yellowtail_400Regular } from '@expo-google-fonts/yellowtail';
+import { Poppins_500Medium } from '@expo-google-fonts/dev';
 import { StyleSheet } from 'react-native';
-
 export default StyleSheet.create({
 
   Text1: {
-    fontFamily: Yellowtail_400Regular,
-    fontWeight: 'bold',
+    fontFamily: 'Yellowtail_400Regular',
     fontSize: 96,
-    fontStyle: 'italic',
     marginTop: '20%',
     textAlign: 'center',
     color: '#DBB364',
   },
   Text2: {
     fontSize: 18,
+    fontFamily: 'Poppins_400Regular',
     color: '#fff',
     textAlign: 'center',
   },
@@ -24,15 +22,65 @@ export default StyleSheet.create({
     borderRadius: 20,
   },
   Text3: {
+    fontFamily: 'Poppins_400Regular',
     color: '#fff',
     fontSize: 15,
     textAlign: 'center',
     marginTop: '4%',
   },
   Text4: {
+    fontFamily: 'Moul_400Regular',
     color: 'black',
     textAlign: 'center',
-    marginTop: '15%',
+    marginTop: '40%',
     fontSize: 16,
+  },
+  Facebook: {
+    backgroundColor: '#1877F2',
+    height: '75%',
+    width: '90%',
+    marginLeft: '6%',
+    borderRadius: 21,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  FacebookText: {
+    color: '#fff',
+    marginLeft: '3%',
+    fontFamily: 'Poppins_500Medium',
+    fontSize: 18,
+  },
+  Google: {
+    backgroundColor: '#FFFFFF',
+    height: '75%',
+    width: '90%',
+    marginTop: '7%',
+    marginLeft: '6%',
+    borderRadius: 21,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  GoogleText: {
+    color: '#000000A6',
+    marginLeft: '3%',
+    fontFamily: 'Poppins_500Medium',
+    fontSize: 18,
+  },
+
+  Apple: {
+    backgroundColor: '#000000',
+    height: '75%',
+    width: '90%',
+    marginTop: '7%',
+    marginLeft: '6%',
+    borderRadius: 21,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  AppleText: {
+    color: '#fff',
+    marginLeft: '3%',
+    fontFamily: 'Poppins_500Medium',
+    fontSize: 18,
   }
 });
