2a9b079c2f0398809936cd5e05362e2b52b417a4
EsadCetin <mehmetesadcetin@gmail.com>
2021-09-17 17:07:55 +0300

    assaas

--- data/firebaseConfig.js
+++ data/firebaseConfig.js
@@ -1,7 +1,6 @@
+// Import the functions you need from the SDKs you need
 import firebase from "firebase";
 import "firebase/firestore";
-
-// Import the functions you need from the SDKs you need
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +8,13 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyALYu0C1xq_76n8Yh57gko1IaG62o_0Cs4",
-    authDomain: "homework-7e737.firebaseapp.com",
-    projectId: "homework-7e737",
-    storageBucket: "homework-7e737.appspot.com",
-    messagingSenderId: "546298178031",
-    appId: "1:546298178031:web:cf953ebb612dafc06c2e1a",
-    measurementId: "G-SB7XRBSBFK"
+    apiKey: "AIzaSyB5ZW2xG4b0mtrz1vCm3RMPwj9cZjaViI4",
+    authDomain: "kral-1279f.firebaseapp.com",
+    projectId: "kral-1279f",
+    storageBucket: "kral-1279f.appspot.com",
+    messagingSenderId: "207160830137",
+    appId: "1:207160830137:web:41bb6dd6bbc20b85201368",
+    measurementId: "G-R1HRPS3N77"
 };
 
 // Initialize Firebase
--- screens/Screen5.js
+++ screens/Screen5.js
@@ -1,43 +1,60 @@
 import { Text, View } from "react-native";
-import styles from "./styles";
+import styles from "../../homework/screens/styles";
 import React, { useState } from "react";
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
-import firebase from '../data/firebaseConfig'
+import firebase from '../../homework/data/firebaseConfig';
+import auth from "@react-native-firebase/auth"
 
 function Screen5({ navigation }) {
 
     const [state, setState] = useState({
         Adı: '',
-        TelefonNumarası: '',
+        Email: '',
+        Parola: '',
         Fotoğrafı: '',
     });
     const changeText = (Adı, value) => {
         setState({ ...state, [Adı]: value })
     }
-
+    const doCreateUser = async () => {
+        try {
+            let response = await auth().createUserWithEmailAndPassword(UserEmail, UserPassword)
+            if (response) {
+                console.log(response)
+                Alert.alert('Welcome Message', 'Welcome the App');
+            }
+        } catch (e) {
+            console.error(e.message)
+        }
+    }
     const addData = () => {
-        if (state.Adı == '') {
-            alert('Lütfen adınızı giriniz')
-        } else if (state.TelefonNumarası == '') {
-            alert('Lütfen Telefon Numaranızı giriniz')
+        if (state.Email == '') {
+            alert('Lütfen E-mail Adresinizi giriniz')
+        } else if (state.Parola == '') {
+            alert('Lütfen Parolanızı giriniz')
+        } else if (state.Adı == '') {
+            alert('Lütfen Adınızı giriniz')
         } else if (state.Fotoğrafı == '') {
             alert('Lütfen Fotoğrafınızı yükleyiniz')
         } else {
             firebase.db.collection('users').add({
+                Email: state.Email,
+                Parola: state.Parola,
                 Adı: state.Adı,
-                TelefonNumarası: state.TelefonNumarası,
                 Fotoğrafı: state.Fotoğrafı,
 
             })
             navigation.navigate('Sixth Screen')
+            doCreateUser();
         }
     }
     return (
         <View style={styles.Screen}>
             <Text style={styles.Text1}>torba</Text>
 
+            <TextInput style={styles.TextInput} placeholder="E-mail Adresiniz" onChangeText={(value) => changeText('Email', value)} keyboardType={"email-address"} />
+            <TextInput style={styles.TextInput} placeholder="Parola" onChangeText={(value) => changeText('Parola', value)} keyboardType={"number-pad"} />
             <TextInput style={styles.TextInput} placeholder="Adınız" onChangeText={(value) => changeText('Adı', value)} />
-            <TextInput style={styles.TextInput} placeholder="Telefon Numaranız" keyboardType="numeric" maxLength={11} onChangeText={(value) => changeText('TelefonNumarası', value)} />
             <TextInput style={styles.TextInput} placeholder="Eklemek İstediğiniz Fotoğraf" onChangeText={(value) => changeText('Fotoğrafı', value)} />
             <TouchableOpacity onPress={() => addData()} style={styles.AddButton}>
                 <Text style={styles.Add}>Ekle</Text>
